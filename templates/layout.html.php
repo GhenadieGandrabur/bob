dec2ac0821cdf9af644bf18132b413111c434239
ghenadie gandrabur <ghenadiegandrabur@gmail.com>
2024-02-21 11:40:55 +0200

    picker date

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -5,9 +5,14 @@
 		<link rel="stylesheet" href="/jokes.css">
 		<title><?=$title?></title>     
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
       <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 		<script src="https://code.jquery.com/ui/1.12.1/i18n/jquery-ui-i18n.min.js"></script>
+		<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+		<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+		<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 		<link rel="stylesheet" type="text/css" href="https://css.cubit.md/css/buttons.css" />
         <link rel="stylesheet" type="text/css" href="https://css.cubit.md/css/form.css" />
         <link rel="stylesheet" type="text/css" href="https://css.cubit.md/css/shortcast.css" />
